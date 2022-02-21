3e32003df0ed564574701e76489b038a038e4dd0
Sasha Czerniawski <sashacandoit@gmail.com>
2022-02-21 11:29:23 -0500

    done

--- serial.py
+++ serial.py
@@ -25,11 +25,13 @@ class SerialGenerator:
         
         self.start = self.next = start 
 
+
     def __repr__(self):
         """Show representation of serial generator"""
         
         return f"SerialGenerator start={self.start} next={self.next}"
     
+
     def generate(self):
         """Return next serial number in sequence"""
 
@@ -39,6 +41,7 @@ class SerialGenerator:
 
         # return value of self.next - 1 to show current serial number
         return self.next - 1
+
 
     def reset(self):
         """Reset number to original start value"""
