825aa3bc10024d95b5bf3c36c5f6b5f7f7cefbeb
Sasha Czerniawski <sashacandoit@gmail.com>
2022-02-21 14:43:24 -0500

    subclass SpecialWordFinder added

--- wordfinder.py
+++ wordfinder.py
@@ -1,25 +1,22 @@
 """Word Finder: finds random words from a dictionary."""
 from random import choice
-
-
-# class WordFinder:
-#     def __init__(self, file):
-#         self.words = []
-#         self.file = file
-
-#         words_file = open(file, "r")
-#         for word in words_file:
-#             self.words.add(word)
-
-#     def __repr__(self):
-#         word_count = len(self.words)
-#         return f"{word_count} words read"
-
-#     def random(self):
-
     
 
 class WordFinder:
+    """Machine for finding random words from dictionary.
+    
+    >>> wf = WordFinder("colors.txt")
+    4 words read
+
+    >>> wf
+    WordFinder file = colors.txt 
+
+    >>> wf.random()
+    yellow
+
+    >>> wf.random() in ['blue', 'green', 'yellow', 'red']
+    True
+    """
 
     def __init__(self, file):
         """
@@ -29,12 +26,17 @@ class WordFinder:
         """
 
         word_file = open(file)
+        self.file = file
         self.words = self.list_words(word_file)
         word_file.close()
 
         word_count = len(self.words)
         print(f"{word_count} words read in file")
     
+
+    def __repr__(self):
+        return f"WordFinder file = {self.file}"
+
 
     def list_words(self, word_file):
         """Add each word from word file into list"""
@@ -44,4 +46,32 @@ class WordFinder:
     
     def random(self):
         """Return random word from list"""
+
         return choice(self.words)
+
+
+class SpecialWordFinder(WordFinder):
+    """
+    Specialized WordFinder that ignores blank lines and comments.
+
+    >>> swf = SpecialWordFinder("complex.txt")
+    8 words read
+
+    >>> swf
+    WordFinder file = complext.txt 
+
+    >>> swf.random()
+    yellow
+
+    >>> swf.random() in ['blue', 'green', 'yellow', 'red']
+    True
+    """
+    
+    def __init__(self, file):
+        super().__init__(file)
+
+    
+    def list_words(self, word_file):
+        """Add each word from word file into list, skipping blanks and comments"""
+        return [word.strip() for word in word_file if word.strip() and not word.startswith('#')]
+
